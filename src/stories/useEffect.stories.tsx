9aeeb505e23a5482156cb71c4be90d0cce0500e8
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-01-16 19:02:25 +0500

    made a clock in useEffect.stories.tsx

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -41,3 +41,49 @@ export const SimpleExample = () => {
         </>
     )
 }
+
+export const setTimeoutExample = () => {
+    console.log("setTimeoutExample")
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    let [dateHourse, setHourseDate] = useState<number>(+new Date().getHours())
+    let [dateMinute, setMinuteDate] = useState<number>(+new Date().getMinutes())
+    let [dateSeconds, setSecondsDate] = useState<number>(+new Date().getSeconds())
+
+    /*let [date, setDate] = useState<Date>()*/
+
+
+    useEffect(() => {
+        //setInterval берет значение из замыкания, которое было при первом запуске, а это 1,
+        //useEffect() больше не регистрируется, тк как нет зависимостей и поэтому все время будет выводиться 2
+        //если вместо (state) => state + 1 написать counter => counter + 1
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+
+        setInterval(() => {
+            setHourseDate(+new Date().getHours())
+        }, 1000)
+
+        setInterval(() => {
+            setMinuteDate(+new Date().getMinutes())
+        }, 1000)
+
+        setInterval(() => {
+            setSecondsDate(+new Date().getSeconds())
+        }, 1000)
+
+    }, [])
+
+    return (
+        <>
+            {/*Hello, Fake: {fake} - Counter: {counter}*/}
+            {`${dateHourse} : ${dateMinute} : ${dateSeconds}`}
+            {/*{date}*/}
+            {/*<button onClick={() => setCounter(counter + 1)}>Counter +</button>
+            <button onClick={() => setFake(fake + 1)}>Fake +</button>*/}
+        </>
+    )
+}
