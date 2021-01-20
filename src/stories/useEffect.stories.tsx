95b09c2ec1595fbc7cea388038d163464ff2acb8
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-01-20 16:54:52 +0500

    added KeysTrackerExample

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -18,16 +18,19 @@ export const SimpleExample = () => {
         //document.title = "User"
     }, [counter]) //прописываем зависимости, чтобы useEffect каждый раз не пеерерисовывался
 
+    //перерисовывается каждый раз
     useEffect(() => {
         console.log("useEffect every render")
         document.title = counter.toString()
     })
 
+    //рисуется только при первой загрузке
     useEffect(() => {
         console.log("useEffect only first render (componentDdMount)")
         document.title = counter.toString()
     }, [])
 
+    //зависит от counter
     useEffect(() => {
         console.log("useEffect first render and every counter change")
         document.title = counter.toString()
@@ -42,6 +45,7 @@ export const SimpleExample = () => {
     )
 }
 
+
 export const KeysTrackerExample = () => {
 
     const [text, setText] = useState('')
@@ -54,6 +58,7 @@ export const KeysTrackerExample = () => {
         }
         window.document.addEventListener('keypress', handler)
 
+        //аналог ComponentWillUnmount
         return () => {
             window.document.removeEventListener('keypress', handler)
         }
@@ -64,17 +69,40 @@ export const KeysTrackerExample = () => {
     </>
 }
 
-/*
-export const setTimeoutExample = () => {
+
+export const setTimeoutExamlpe = () => {
+
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        const interval = setTimeout(() => {
+            setText('3 seconds')
+        }, 3000)
+
+        //аналог ComponentWillUnmount
+        return () => {
+            clearInterval(interval)
+        }
+    }, [text])
+
+    return <>
+        Typed text: {text}
+    </>
+}
+
+
+//императорский говнокод
+//не воспринимать всерьез
+export const setIntervalExample = () => {
     console.log("setTimeoutExample")
 
-    const [counter, setCounter] = useState(1)
+    let [counter, setCounter] = useState(1)
 
     let [dateHourse, setHourseDate] = useState<number>(+new Date().getHours())
     let [dateMinute, setMinuteDate] = useState<number>(+new Date().getMinutes())
     let [dateSeconds, setSecondsDate] = useState<number>(+new Date().getSeconds())
 
-    /!*let [date, setDate] = useState<Date>()*!/
+    /*let [date, setDate] = useState<Date>()*/
 
     useEffect(() => {
         //setInterval берет значение из замыкания, которое было при первом запуске, а это 1,
@@ -84,27 +112,29 @@ export const setTimeoutExample = () => {
             setCounter((state) => state + 1)
         }, 1000)
 
-        setInterval(() => {
+        const hoursInterval = setInterval(() => {
             setHourseDate(+new Date().getHours())
         }, 1000)
 
-        setInterval(() => {
+        const minuteInterval = setInterval(() => {
             setMinuteDate(+new Date().getMinutes())
         }, 1000)
 
-        setInterval(() => {
+        const secondInterval = setInterval(() => {
             setSecondsDate(+new Date().getSeconds())
         }, 1000)
+
+        return () => {
+            clearInterval(hoursInterval)
+            clearInterval(minuteInterval)
+            clearInterval(secondInterval)
+        }
 
     }, [])
 
     return (
         <>
-            {/!*Hello, Fake: {fake} - Counter: {counter}*!/}
             {`${dateHourse} : ${dateMinute} : ${dateSeconds}`}
-            {/!*{date}*!/}
-            {/!*<button onClick={() => setCounter(counter + 1)}>Counter +</button>
-            <button onClick={() => setFake(fake + 1)}>Fake +</button>*!/}
         </>
     )
-}*/
+}
